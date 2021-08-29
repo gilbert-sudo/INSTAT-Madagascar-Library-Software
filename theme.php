39aa3d881490b099f2d6362574e1947d93bc23bf
gilbert-sudo <mikamickswonder@gmail.com>
2021-08-29 10:32:47 +0300

    first

---
+++ theme.php
@@ -0,0 +1,13 @@
+color="#2f4a5d"
+color="#5F6C75"
+color="#2F495C"
+color="#889BA8"
+color="#7DC3F5"
+
+color="#BF0000"
+<?php 
+echo '<script type="text/javascript">
+alert("bonjour");
+</script>';
+?>
+
